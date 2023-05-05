260420f9bd90ef4925755ee22291a83a1c8c7f41
Ryan Bradley <ryanbradley315@gmail.com>
2023-05-05 16:18:05 -0500

    Add home page content and start basic styling.

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,33 @@
-
+import envelope from '../../assets/envelope-regular.svg'
+import styles from './Home.module.css'
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <section className={styles.heroSection}>
+
+            <h1 className={styles.hero}>
+                DON'T LET
+                <br />
+                A <span>BAD MIX</span>
+                <br />
+                DROWN OUT
+                <br />
+                <span>GREAT MUSIC</span>
+            </h1>
+
+            <p>
+                My mixes help your recordings cut through the noise
+            </p>
+
+            <button className={styles.heroBtnTop}>
+                <img src={envelope} alt="" />
+                Get In Touch
+            </button>
+
+            <button className={styles.heroBtnBottom}>
+                Hear My Work
+            </button>
+
+        </section>
     )
 }
