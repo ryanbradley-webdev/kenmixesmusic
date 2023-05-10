56fe5cf915bafb2b6b75c8a85ee453d4a4895a39
Ryan Bradley <ryanbradley315@gmail.com>
2023-05-10 16:33:18 -0500

    Initialized firebase hosting.

--- src/components/Credits/Credits.tsx
+++ src/components/Credits/Credits.tsx
@@ -1,7 +1,16 @@
 
 
 export default function Credits() {
+    const localStyles = {
+        display: 'grid',
+        placeItems: 'center',
+        height: '100%',
+        fontSize: '30px'
+    }
+
     return (
-        <div>Credits</div>
+        <div style={localStyles}>
+            I haven't figured this one out yet.<br />Chill out it's coming.
+        </div>
     )
 }
