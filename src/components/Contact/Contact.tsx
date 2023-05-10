ca5a050b0aa0164fab7b2fd32d669546720b61cf
Ryan Bradley <ryanbradley315@gmail.com>
2023-05-10 13:51:57 -0500

    Add contact form UI for invalid inputs and form submission.

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react'
 import CloseBtn from '../../assets/CloseBtn'
 import styles from './Contact.module.css'
 
@@ -7,29 +8,131 @@ type ContactProps = {
 }
 
 export default function Contact({ isVisible, toggleModal }: ContactProps) {
+    const [formIsValid, setFormIsValid] = useState(true)
+
+    const firstNameRef = useRef<HTMLInputElement>(null)
+    const lastNameRef = useRef<HTMLInputElement>(null)
+    const emailRef = useRef<HTMLInputElement>(null)
+    const messageRef = useRef<HTMLTextAreaElement>(null)
+    const formRef = useRef<HTMLFormElement>(null)
+
+    const validateInput = (ref: React.RefObject<HTMLInputElement | HTMLTextAreaElement>) => {
+        if (!ref || !ref.current) return false
+
+        if (ref.current.value === '') {
+            ref.current.style.backgroundColor = 'var(--color-input-invalid)'
+            ref.current.style.borderBottomColor = 'red'
+            return false
+        } else {
+            ref.current.style.backgroundColor = ''
+            ref.current.style.borderBottomColor = ''
+            return true
+        }
+    }
+
+    const validateForm = () => {
+        const formRefs = [firstNameRef, lastNameRef, emailRef, messageRef]
+
+        formRefs.forEach(ref => {
+            if (!validateInput(ref)) {
+                setFormIsValid(false)
+            }
+        })
+    }
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        if (formIsValid) {
+            const firstName = firstNameRef?.current?.value
+            const lastName = lastNameRef?.current?.value
+            const email = emailRef?.current?.value
+            const message = messageRef?.current?.value
+
+            const formData = {
+                name: firstName + ' ' + lastName,
+                email,
+                message
+            }
+
+            formRef?.current?.reset()
+            return formData //TODO add form submission logic
+        }
     }
 
     return (
         <div className={styles.modal} style={{ display: isVisible ? 'grid' : 'none' }}>
-            <form action="" onSubmit={handleSubmit} className={styles.form}>
+
+            <form action="" onSubmit={handleSubmit} className={styles.form} ref={formRef}>
+
                 <h2>GET IN TOUCH</h2>
+
                 <CloseBtn className={styles.closeBtn} toggleMenu={toggleModal} />
+
                 <div className={styles.namesGrid}>
-                    <label htmlFor="first-name">First Name *</label>
-                    <input type="text" id='first-name' name='first-name' placeholder='e.g. Kat' required />
-                    <label htmlFor="last-name">Last Name *</label>
-                    <input type="text" id='last-name' name='last-name' placeholder='e.g. Jones' required />
+
+                    <label htmlFor="first-name">
+                        First Name *
+                    </label>
+                    <input 
+                        type="text"
+                        id='first-name'
+                        name='first-name'
+                        placeholder='e.g. Kat'
+                        onBlur={() => validateInput(firstNameRef)}
+                        onChange={() => validateInput(firstNameRef)}
+                        ref={firstNameRef}
+                        required
+                    />
+
+                    <label htmlFor="last-name">
+                        Last Name *
+                    </label>
+                    <input
+                        type="text"
+                        id='last-name'
+                        name='last-name'
+                        placeholder='e.g. Jones'
+                        onBlur={() => validateInput(lastNameRef)}
+                        onChange={() => validateInput(lastNameRef)}
+                        ref={lastNameRef}
+                        required
+                    />
+
                 </div>
-                <label htmlFor="email">Email *</label>
-                <input type="email" id='email' name='email' placeholder='e.g. example@mail.com' required />
-                <label htmlFor="message">Message *</label>
-                <textarea id='message' name='message' placeholder='Type your message here' required></textarea>
-                <button className={styles.submit}>
+
+                <label htmlFor="email">
+                    Email *
+                </label>
+                <input
+                    type="email"
+                    id='email'
+                    name='email'
+                    placeholder='e.g. example@mail.com'
+                    onBlur={() => validateInput(emailRef)}
+                    onChange={() => validateInput(emailRef)}
+                    ref={emailRef}
+                    required
+                />
+
+                <label htmlFor="message">
+                    Message *
+                </label>
+                <textarea
+                    id='message'
+                    name='message'
+                    placeholder='Type your message here'
+                    onBlur={() => validateInput(messageRef)}
+                    onChange={() => validateInput(messageRef)}
+                    ref={messageRef}
+                    required
+                ></textarea>
+
+                <button className={styles.submit} onClick={validateForm}>
                     Submit
                 </button>
+
             </form>
+
         </div>
     )
 }
